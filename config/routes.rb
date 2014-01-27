a9aa57239ae66a749f6e5a87362b2cb115bbd026
hannibal80 <anibalcaixinha@gmail.com>
2014-01-27 23:58:46 +0000

    New Reader View Spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ AcLibapp::Application.routes.draw do
 
   get "/register", to:"readers#new", as: "register"
 
+  resources :readers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
