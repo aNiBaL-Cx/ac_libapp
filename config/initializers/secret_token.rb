4acb37be55e30e708af488ce9964c8f7a5692d39
hannibal80 <anibalcaixinha@gmail.com>
2014-01-27 21:50:31 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AcLibapp::Application.config.secret_key_base = '4fbbba5b16422a3c21b720827c67665f4ee76c79fe0ff69c29d4dc8699b0b66dc908a7a0aac993d539bd0f033f9f3eea1ce10ca969c09b3fd88232f8188e6d63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AcLibapp::Application.config.session_store :cookie_store, key: '_ac_libapp_session'
